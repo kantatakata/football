f21cd11796d970795004828916f7fca50ee631f9
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-12-05 14:22:01 +0000

    model create

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+end
+
