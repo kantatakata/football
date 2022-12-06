0c2b2368f6457dd5fb5fe8591eb910d385405bdc
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-12-06 13:56:01 +0000

    bootstrap add

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,48 @@
 Rails.application.routes.draw do
-  # 顧客用
-# URL /customers/sign_in ...
-devise_for :customers, controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
+namespace :admin do
+  get 'top' => 'homes#top', as: 'top'
+  get 'search' => 'homes#search', as: 'search'
+  get 'customers/:customer_id/orders' => 'orders#index', as: 'customer_orders'
+  resources :customers, only: [:index, :show, :edit, :update]
+  resources :items, except: [:destroy]
+  resources :genres, only: [:index, :create, :edit, :update]
+  resources :orders, only: [:index, :show, :update] do
+  resources :order_details, only: [:update]
+    end
+  end# URL /customers/sign_in ...
+devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+ scope module: :public do
+    root 'homes#top'
+    
+    get 'customers/mypage' => 'customers#show', as: 'mypage'
+    # customers/editのようにするとdeviseのルーティングとかぶってしまうためinformationを付け加えている。
+    get 'customers/information/edit' => 'customers#edit', as: 'edit_information'
+    patch 'customers/information' => 'customers#update', as: 'update_information'
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
+    put 'customers/information' => 'customers#update'
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all_cart_items'
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/confirm' => 'orders#error'
+    get 'orders/thanks' => 'orders#thanks', as: 'thanks'
+    
+    resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+    resources :items, only: [:index, :show] do
+      resources :cart_items, only: [:create, :update, :destroy]
+    end
+    resources :cart_items, only: [:index]
+    resources :orders, only: [:new, :index, :create, :show]
+    
+  end
+
+
 end
 
