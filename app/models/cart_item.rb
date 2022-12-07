27477a5f2ebb96e215cc03f95cc63cafeee25b47
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-12-07 15:53:30 +0000

    モデル　追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,15 @@
 class CartItem < ApplicationRecord
+  
+  belongs_to :customer
+  belongs_to :item
+
+  validates :item_id, uniqueness: { scope: :customer_id }
+  # 数値のバリデーション
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+
+  def subtotal
+    item.with_tax_price * amount
+  end
+  
+
 end
