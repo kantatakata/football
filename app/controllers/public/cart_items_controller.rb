47dabd7db50c09860f7f626e8a2f20fae5f8fc23
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-12-13 09:10:34 +0000

    cart_item index create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,11 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
   before_action :set_cart_item, only: [:create, :update, :destroy]
-  
+
   def index
      @cart_items = current_customer.cart_items.includes(:item)
   end
-  
+
   def create
     if  @cart_item
        new_amount = @cart_item.amount + cart_item_params[:amount]
@@ -21,28 +21,28 @@ class Public::CartItemsController < ApplicationController
        end
     end
   end
-  
+
   def update
     @cart_item.update(cart_item_params) if @cart_item
     redirect_to cart_items_path
   end
-  
+
   def destroy
     @cart_item.destroy if @cart_item
     redirect_to cart_items_path
   end
-  
+
   def destroy_all
     current_customer.cart_items.destroy_all
     redirect_to cart_items_path
   end
-  
+
   private
-  
+
   def cart_item_params
     params.require(:cart_item).permit(:amount)
   end
-  
+
   def set_cart_item
     @item = Item.find(params[:item_id])
     @cart_item = current_customer.has_in_cart(@item)
