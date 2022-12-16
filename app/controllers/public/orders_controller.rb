0969957ee7fb173c5c321ead713fafe135764fbe
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-12-16 12:58:54 +0000

    order controller create

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,8 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :ensure_cart_items, only: [:new, :confirm, :create, :error]
+
+  def new
+    @order = Order.new
+  end
 end
