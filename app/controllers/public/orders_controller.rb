303c4e8f459a87bfaceef36b5d87772590128e0b
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-12-17 15:07:27 +0000

    order controller create

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,6 +6,21 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
   
+  def confirm
+    @order = Order.new(order_params)
+    if params[:select_address] == '0'
+      @order.get_shipping_informations_from(current_customer)
+    elsif params[:select_address] == '1'
+      @selected_address = current_customer.addresses.find(params[:address_id])
+      @order.get_shipping_informations_from(@selected_address)
+    elsif params[:select_address] == '2' && (@order.postal_code =~ /\A\d{7}\z/) && @order.destination? && @order.name?
+      # 処理なし
+    else
+      flash[:error] = '情報を正しく入力して下さい。'
+      render :new
+    end
+  end
+  
   def error
   end
   
