11f28ecd6e02afdc0df21c6f1abeaf9c69c5ab63
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-12-17 15:08:18 +0000

    order controller create

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -23,5 +23,17 @@ class Public::OrdersController < ApplicationController
   
   def error
   end
+
+  def create
+    @order = current_customer.orders.new(order_params)
+    @order.shipping_cost = 800
+    @order.grand_total = @order.shipping_cost + @cart_items.sum(&:subtotal)
+    if @order.save
+      @order.create_order_details(current_customer)
+      redirect_to thanks_path
+    else
+      render :new
+    end
+  end
   
 end
